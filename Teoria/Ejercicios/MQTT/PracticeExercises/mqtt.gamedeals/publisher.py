e6f6fd4fcb894b41c83f95f3f7f203fd590756d9
PabloBlazquezSanchez <Pablo.Blazquez1@alu.uclm.es>
2025-11-13 11:44:38 +0100

    Agregar implementación del sistema de notificación de ofertas de videojuegos utilizando MQTT, incluyendo publisher, subscriber, y archivo de requisitos.

---
+++ Teoria/Ejercicios/MQTT/PracticeExercises/mqtt.gamedeals/publisher.py
@@ -0,0 +1,3 @@
+from videogames import deals
+
+#TODO: Implement the MQTT publisher that sends the game deals to a specific topic.
---
+++ Teoria/Ejercicios/MQTT/PracticeExercises/mqtt.gamedeals/subscriber.py
@@ -0,0 +1,2 @@
+
+#TODO: Implement the MQTT subscriber that listens to the game deals topic and processes incoming messages.
